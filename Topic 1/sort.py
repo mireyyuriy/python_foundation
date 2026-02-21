a30f4782989b43630aa24a2a0ccce972fe126657
mireyyuriy <78160545+mireyyuriy@users.noreply.github.com>
2026-02-21 20:37:50 +0200

    added functions

--- Topic 1/sort.py
+++ Topic 1/sort.py
@@ -3,3 +3,18 @@ nums.sort()
 print(nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
 nums.sort(reverse=True)
 print(nums)  # Виведе [9, 5, 4, 3, 2, 1, 1]
+
+words = ["banana", "apple", "cherry"]
+words.sort(key=len)
+print(words)  # Виведе ['apple', 'banana', 'cherry']
+
+nums = [3, 1, 4, 1, 5, 9, 2]
+sorted_nums = sorted(nums)
+print(sorted_nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
+
+sorted_nums_desc = sorted(nums, reverse=True)
+print(sorted_nums_desc)  # Виведе [9, 5, 4, 3, 2, 1, 1]
+
+words = ["banana", "apple", "cherry"]
+sorted_words = sorted(words, key=len)
+print(sorted_words)  # Виведе ['apple', 'banana', 'cherry']
